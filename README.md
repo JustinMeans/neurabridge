83796a5f865c9cf7af10e808b0607366e60c9ce5
Justin Means <justincmeans@gmail.com>
2021-05-28 11:43:12 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# neurabridge
+
+A description of this package.
---
+++ Tests/neurabridgeTests/neurabridgeTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import neurabridge
+
+    final class neurabridgeTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(neurabridge().text, "Hello, World!")
+        }
+    }
